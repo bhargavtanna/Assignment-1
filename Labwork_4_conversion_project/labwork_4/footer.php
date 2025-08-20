72e6c58ff2d9db222896803e9bdd40f96da861bf
bhargavtanna <bhargav.tanna142598@marwadiuniversity.ac.in>
2025-08-20 11:58:53 +0530

    Add files via upload

---
+++ Labwork_4_conversion_project/labwork_4/footer.php
@@ -0,0 +1,11 @@
+<footer id="footer">
+						<p>&copy; Untitled. All rights reserved. Design: <a href="http://templated.co">TEMPLATED</a>. Demo Images: <a href="http://unsplash.com">Unsplash</a>.</p>
+					</footer>
+
+					<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ Labwork_4_conversion_project/labwork_4/index.php
@@ -0,0 +1,45 @@
+<?php include("header.php")?>
+
+			<div id="wrapper">
+					<section id="main">
+							<section class="thumbnails">
+								<div>
+									<a href="images/fulls/01.jpg">
+										<img src="images/thumbs/01.jpg" alt="" />
+										<h3>tall building</h3>
+									</a>
+									<a href="images/fulls/02.jpg">
+										<img src="images/thumbs/02.jpg" alt="" />
+										<h3>a person</h3>
+									</a>
+								</div>
+								<div>
+									<a href="images/fulls/03.jpg">
+										<img src="images/thumbs/03.jpg" alt="" />
+										<h3>train station</h3>
+									</a>
+									<a href="images/fulls/04.jpg">
+										<img src="images/thumbs/04.jpg" alt="" />
+										<h3>mountain</h3>
+									</a>
+									<a href="images/fulls/05.jpg">
+										<img src="images/thumbs/05.jpg" alt="" />
+										<h3>legs in the sky</h3>
+									</a>
+								</div>
+								<div>
+									<a href="images/fulls/06.jpg">
+										<img src="images/thumbs/06.jpg" alt="" />
+										<h3>snowy place</h3>
+									</a>
+									<a href="images/fulls/07.jpg">
+										<img src="images/thumbs/07.jpg" alt="" />
+										<h3>lake</h3>
+									</a>
+								</div>
+							</section>
+
+					</section>
+			</div>
+		
+<?php include('footer.php'); ?>
